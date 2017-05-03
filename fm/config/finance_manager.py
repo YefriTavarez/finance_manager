180f8705e19b33433c9e75f8fd1812e54b5307fb
Yefri Tavarez <yefritavarez@hotmail.com>
2017-05-03 09:56:33 -0400

    Add Links access to other docs in the system

---
+++ fm/config/finance_manager.py
@@ -0,0 +1,66 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Docs"),
+			"icon": "fa fa-star",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Loan Application",
+					"description": _("Application from cutomers"),
+				},
+				{
+					"type": "doctype",
+					"name": "Loan",
+					"description": _("Loan and repayment schedules"),
+				},
+			]
+		},
+		{
+			"label": _("Customers"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Customer",
+					"description": _("Customer database."),
+				},
+				{
+					"type": "doctype",
+					"label": _("Customer Group"),
+					"name": "Customer Group",
+					"icon": "fa fa-sitemap",
+					"link": "Tree/Customer Group",
+					"description": _("Manage Customer Group Tree."),
+				},
+				{
+					"type": "doctype",
+					"name": "Contact",
+					"description": _("All Contacts."),
+				},
+				{
+					"type": "doctype",
+					"name": "Contact",
+					"label": "Refencias",
+					"description": _("All References"),
+				},
+				{
+					"type": "doctype",
+					"name": "Address",
+					"description": _("All Addresses."),
+				},
+
+			]
+		},
+		{
+			"label": _("Vehiculos"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Vehiculo"
+				}
+			]
+		}
+	]
