79d7f866455bdc3330f3da4f8aaee246d493bd56
Yefri Tavarez <yefritavarez@hotmail.com>
2017-05-11 12:21:12 -0400

    Optimize the scheduler job calculate_fines in scheduler.py

--- fm/scheduler.py
+++ fm/scheduler.py
@@ -4,7 +4,7 @@ from math import ceil
 
 def calculate_fines():
 	print "================================================================="
-	print "**** Executing job"
+	print "********** Executing job -> calculate_fines **********"
 	print "================================================================="
 	# global defaults
 	fine = frappe.db.get_single_value("FM Configuration", "vehicle_fine")
@@ -13,22 +13,41 @@ def calculate_fines():
 	today = date.today()
 
 	# let's begin
-	for loan in frappe.get_list("Loan",{"docstatus":1,"status":["=","Fully Disbursed"]}):
-		doc = frappe.get_doc("Loan",loan.name)
-		print "Loan -> {}".format(doc.name)
+	for loan in frappe.get_list("Loan", {"docstatus":1, "status": "Fully Disbursed" }):
+		print "************** Evaluating Loan -> {} **************".format(loan.name)
+		print "-----------------------------------------------------------------"
+
+		doc = frappe.get_doc("Loan", loan.name) # load from db
+
 		for row in doc.get("repayment_schedule"):
+			print "Evaluating Loan row -> {}".format(row.idx)
+
 			# date diff in days
 			date_diff = frappe.utils.date_diff(today, row.fecha)
-			if row.estado == "PENDIENTE" and frappe.utils.add_days(row.fecha, int(grace_days)) < today:
-				print "estado -> {}".format(row.estado)
-				print "row fecha -> {}".format(frappe.utils.add_days(row.fecha, int(grace_days)))
-				print "today -> {}".format(today)
-				print row.estado
-				if not (fine_rate * doc.monthly_repayment_amount * ceil(date_diff / 30.0) == row.fine:
-					row.fine = fine_rate * doc.monthly_repayment_amount * ceil(date_diff / 30.0) 
-					doc.save()
+			due_payments = ceil(date_diff / 30.0)
+			due_date = frappe.utils.add_days(row.fecha, 0 if due_payments > 1 else int(grace_days))
+			new_fine = fine_rate * doc.monthly_repayment_amount * due_payments
+
+			if row.estado == "PENDIENTE" and today > due_date:
+				print "Row {} is already due with {} months".format(row.idx, due_payments)
+
+				if not ceil(new_fine) == float(row.fine):
+					print "Row {} is out of date. Updating!".format(row.idx)
+
+					row.fine = ceil(new_fine) # setting the new fine
+					doc.db_update() # updating the document
 					
 					print "amount -> {}".format(doc.monthly_repayment_amount)
+					print "due_payments -> {}".format(due_payments)
 					print "fine rate -> {}".format(fine_rate)
 					print "fine -> {}".format(row.fine)
+				else:
+					print "Row {} is up to date. Nothing to do!".format(row.idx)
+			else:
+				print "Row {} is fine. Skipping!".format(row.idx)
+
+			print "-----------------------------------------------------------------"
+
+		print "*****************************************************************"
+
 	print "================================================================="
