0f58924340924625a2d70fa038cb2b674b6bbe47
Lewin Villar <lewin.villar@gmail.com>
2017-05-12 23:04:44 -0700

    Agregar Validacion allocate_to_email FM Configuration

--- fm/finance_manager/doctype/fm_configuration/fm_configuration.js
+++ fm/finance_manager/doctype/fm_configuration/fm_configuration.js
@@ -4,5 +4,21 @@
 frappe.ui.form.on('FM Configuration', {
 	refresh: function(frm) {
 
+	},
+	allocated_to_email: function (frm) {
+		if(frm.doc.allocated_to_email=="") return;
+		
+		frappe.model.get_value('User', {'email': frm.doc.allocated_to_email}, 'email',
+		  	function(d) {
+		   	if(d)
+		   	{
+		   		frm.set_value("allocated_to_email",d.email);
+		   	}	
+		   	else
+		   	{
+		   		frm.set_value("allocated_to_email","");
+		   		frappe.msgprint("No existe ningun usuario con el correo ingresado. Debe ingresar un usuario valido");
+		   	}	
+		});
 	}
 });
