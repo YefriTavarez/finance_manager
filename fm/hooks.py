c13a8a45b5e04f601f399fdabb853fa1b0d26daa
Yefri Tavarez <yefritavarez@hotmail.com>
2017-05-22 14:23:19 -0400

    Fixtures exported

--- fm/hooks.py
+++ fm/hooks.py
@@ -11,6 +11,8 @@ app_color = "#496"
 app_email = "servicios@soldeva.com"
 app_license = "General Public Licence v3"
 
+fixtures = ["Custom Script", "Custom Field", "Role", "Print Format"]
+
 # Includes in <head>
 # ------------------
 
